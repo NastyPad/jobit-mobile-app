4595d6ec228a6461d2bbfa2da6cce02fda250db7
Jesús Pacheco <jesuspacheco0219@gmail.com>
2022-11-11 22:53:23 -0500

    add screen and widget items plan

---
+++ lib/screens/plan_premiun_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:jobit_mobile_app/widgets/subscriptions/plan_item.dart';
+
+class planPremiun extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext){
+    return Column(
+         children: [
+           Row(
+             mainAxisAlignment: MainAxisAlignment.center,
+             children: <Widget>[
+               Text(
+                   "Sé un usuario\nPremiun",
+                   style: TextStyle(
+                     color: Colors.amber,
+                     fontSize: 25.0,
+                   )
+               )
+             ],
+           ),
+           Tex
+         ],
+
+    );
+  }
+
+}
--- lib/screens/plans_screen.dart
+++ lib/screens/plans_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:jobit_mobile_app/widgets/subscriptions/plan_item.dart';
 
 class plans extends StatelessWidget{
 
   @override
   Widget build(BuildContext){
     return Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: <Widget>[
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -38,10 +39,36 @@ class plans extends StatelessWidget{
             "Conoce nuestros Planes",
             style: TextStyle(
               color: Colors.amber,
-              fontSize: 15.0,
+              fontSize: 25.0,
             )
-        )
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            plan("Plan Semestral","270 dolares"),
+            plan("Plan Trimestral","530 dolares"),
+          ],
+        ),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children:[
+            plan("Plan Mensual","100 dolares"),
+            ElevatedButton(
+                style: TextButton.styleFrom(
+                     backgroundColor: Colors.amber,
+                  minimumSize: Size(150.0,90.0)
+                ),
+                onPressed:(){
 
+                }, child:
+                Text("ADQUIRIR" ,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25.0,
+                    )                ))
+          ]
+
+        )
       ]
 
     );
