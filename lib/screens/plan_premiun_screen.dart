c5f193b7e84932340bfc853ac5773510dcf292c5
Jesús Pacheco <jesuspacheco0219@gmail.com>
2022-11-11 23:25:04 -0500

    add implement logic premiun app

--- lib/screens/plan_premiun_screen.dart
+++ lib/screens/plan_premiun_screen.dart
@@ -7,7 +7,8 @@ class planPremiun extends StatelessWidget{
   @override
   Widget build(BuildContext){
     return Column(
-         children: [
+
+      children: [
            Row(
              mainAxisAlignment: MainAxisAlignment.center,
              children: <Widget>[
@@ -15,11 +16,55 @@ class planPremiun extends StatelessWidget{
                    "Sé un usuario\nPremiun",
                    style: TextStyle(
                      color: Colors.amber,
-                     fontSize: 25.0,
+                     fontSize: 35.0,
                    )
                )
              ],
            ),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.center,
+             children: <Widget>[
+              Image.network("https://cdn1.vectorstock.com/i/1000x1000/98/85/boy-young-programming-icon-cartoon-style-vector-32359885.jpg",
+              width: 180.0,),
+               Text(
+                   "Aparece en las\nprimeras\nposiciones de\n profesionales",
+                   style: TextStyle(
+                     color: Colors.black,
+                     fontSize: 20.0,
+                   )
+               )
+             ],
+           ),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.center,
+             children: <Widget>[
+               plan("Plan Mensual", "60 dolares"),
+               Text(
+                   "Sé recomendado a\n empresas de acuerdo a tus\nhabilidades y talento",
+                   style: TextStyle(
+                     color: Colors.black,
+                     fontSize: 20.0,
+                   )
+               )
+             ],
+           ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            ElevatedButton(
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.amber,
+                ),
+                onPressed:(){
+
+                }, child:
+            Text("ADQUIRIR" ,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25.0,
+                )                ))
+          ],
+        )
          ],
 
     );
